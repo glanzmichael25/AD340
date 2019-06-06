7414ca9fd0b9b173cf6a3b21d084bb8a5419ec9e
glanzmichael25 <glanz.michael.25@gmail.com>
2019-06-06 15:44:28 -0700

    tests

---
+++ HW5/app/src/test/java/com/j/testapp/bookAppUITest.java
@@ -0,0 +1,34 @@
+package com.j.testapp;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class bookAppUITest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testBookFeatures(){
+        onView(withID(R.id.simple_spinner_dropdown_item))
+                .preformClick(click());
+        onData(allOf(is(instanceOf(String.class)), is("The Hobbit")))
+                .perform(click());
+
+        //onView(withID(R.id.edit_text_book_quantity))
+
+        onView(withID(R.id.button_show_cost)).perform(click());
+
+    }
+
+}
